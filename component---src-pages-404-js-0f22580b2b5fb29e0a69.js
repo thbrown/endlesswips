08255a0250a5b372ca9e766cd5539c0a79bf530b
thbrown <thomasaustenbrown@gmail.com>
2020-10-26 23:57:08 -0600

    Initial commit

---
+++ component---src-pages-404-js-0f22580b2b5fb29e0a69.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,{location:"404"},l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("div",{style:{textAlign:"center"}},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0f22580b2b5fb29e0a69.js.map
