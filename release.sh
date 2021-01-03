941e2ab988c40c535428cc8dca1814607b019804
thbrown <thomasaustenbrown@gmail.com>
2021-01-02 18:03:42 -0700

    Update release script to include CNAME file

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ echo "Copying files from 'endlesswips-private' to 'endlesswips'"
 cp -r ./public/* ../endlesswips/
 echo "Committing changes"
 cd ../endlesswips
+echo "endlesswips.com" >> CNAME
 git add -A
 git commit -m "Update page"
 git push
