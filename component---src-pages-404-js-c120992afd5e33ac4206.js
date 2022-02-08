2cddfe2faaeb5bdd081023959233574399bb3dca
Thomas Brown <thomasaustenbrown@gmail.com>
2022-02-07 17:46:11 -0700

    Update page

--- component---src-pages-404-js-c120992afd5e33ac4206.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,{location:{pathname:"/404"}},l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("div",{style:{textAlign:"center"}},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c120992afd5e33ac4206.js.map
